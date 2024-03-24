297254b7c2c4fa3045a49812bc8388229efc6ba0
Tirtha Jyoti Bol <83816482+TirthaJyotiBol@users.noreply.github.com>
2024-03-24 11:19:16 +0530

    updated to mongodb atlas

--- src/config.mongo.js
+++ src/config.mongo.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-let uri = `mongodb://127.0.0.1:27017/habit`;
+let pass = 'Tirtha555@';
+let uri = `mongodb+srv://tirthabol555:${pass}@cluster0.iyguytz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/habit`;
+
+// let uri = `mongodb://127.0.0.1:27017/habit`;
 
 let connect = async () =>{
     try{
